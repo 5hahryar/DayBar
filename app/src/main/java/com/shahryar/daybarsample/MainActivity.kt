637062d9d4a1cf3d20eaa5545f026f62d8a25c05
Shahryar Khosravi <36692416+5hahryar@users.noreply.github.com>
2020-10-07 12:09:47 +0330

    Pre release

--- app/src/main/java/com/shahryar/daybarsample/MainActivity.kt
+++ app/src/main/java/com/shahryar/daybarsample/MainActivity.kt
@@ -16,7 +16,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         dayBar = findViewById(R.id.dayBar)
-
+        dayBar?.setIndicationByIndex(listOf(2, 6))
         dayBar?.setOnDayChangedListener(object: DayBar.OnDayChangedListener {
             override fun onSelectedDayChanged(date: HashMap<String, String>, chip: DayBarChip) {
                 textView.text = "${chip.isChecked}\n${date}"
